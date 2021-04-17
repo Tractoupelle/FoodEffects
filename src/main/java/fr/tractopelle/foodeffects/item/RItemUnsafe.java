d1f866ec9f25febe5acb86a0a39c3013a723e591
Tractoupelle <unconfigured@null.spigotmc.org>
2021-04-17 18:24:00 +0200

    Add NBT utils

--- src/main/java/fr/tractopelle/foodeffects/item/RItemUnsafe.java
+++ src/main/java/fr/tractopelle/foodeffects/item/RItemUnsafe.java
@@ -2,76 +2,76 @@ package fr.tractopelle.foodeffects.item;
 
 public class RItemUnsafe {
 
-    private ItemBuilder RItemBuilder;
+    private ItemBuilder itemBuilder;
 
-    public RItemUnsafe(ItemBuilder RItemBuilder) {
-        this.RItemBuilder = RItemBuilder;
+    public RItemUnsafe(ItemBuilder itemBuilder) {
+        this.itemBuilder = itemBuilder;
     }
 
     public String getString(String tag){
-        return new fr.tractopelle.foodeffects.item.RNBItem(this.RItemBuilder.toItemStack()).getString(tag);
+        return new fr.tractopelle.foodeffects.item.RNBItem(this.itemBuilder.toItemStack()).getString(tag);
     }
 
     public RItemUnsafe setString(String tag, String value){
-        this.RItemBuilder = new ItemBuilder(new fr.tractopelle.foodeffects.item.RNBItem(this.RItemBuilder.toItemStack()).setString(tag, value).build());
+        this.itemBuilder = new ItemBuilder(new fr.tractopelle.foodeffects.item.RNBItem(this.itemBuilder.toItemStack()).setString(tag, value).build());
         return this;
     }
 
     public RItemUnsafe setByte(String tag, byte b){
-        this.RItemBuilder = new ItemBuilder(new fr.tractopelle.foodeffects.item.RNBItem(this.RItemBuilder.toItemStack()).setByte(tag, b).build());
+        this.itemBuilder = new ItemBuilder(new fr.tractopelle.foodeffects.item.RNBItem(this.itemBuilder.toItemStack()).setByte(tag, b).build());
         return this;
     }
 
     public byte getByte(String tag){
-        return new RNBItem(this.RItemBuilder.toItemStack()).getByte(tag);
+        return new RNBItem(this.itemBuilder.toItemStack()).getByte(tag);
     }
 
     public RItemUnsafe setShort(String tag, short b){
-        this.RItemBuilder = new ItemBuilder(new fr.tractopelle.foodeffects.item.RNBItem(this.RItemBuilder.toItemStack()).setShort(tag, b).build());
+        this.itemBuilder = new ItemBuilder(new fr.tractopelle.foodeffects.item.RNBItem(this.itemBuilder.toItemStack()).setShort(tag, b).build());
         return this;
     }
 
     public short getShort(String tag){
-        return new RNBItem(this.RItemBuilder.toItemStack()).getShort(tag);
+        return new RNBItem(this.itemBuilder.toItemStack()).getShort(tag);
     }
 
     public RItemUnsafe setInt(String tag, int b){
-        this.RItemBuilder = new ItemBuilder(new RNBItem(this.RItemBuilder.toItemStack()).setInt(tag, b).build());
+        this.itemBuilder = new ItemBuilder(new RNBItem(this.itemBuilder.toItemStack()).setInt(tag, b).build());
         return this;
     }
 
     public int getInt(String tag){
-        return new RNBItem(this.RItemBuilder.toItemStack()).getInt(tag);
+        return new RNBItem(this.itemBuilder.toItemStack()).getInt(tag);
     }
 
     public RItemUnsafe setByteArray(String tag, byte[] b){
-        this.RItemBuilder = new ItemBuilder(new RNBItem(this.RItemBuilder.toItemStack()).setByteArray(tag, b).build());
+        this.itemBuilder = new ItemBuilder(new RNBItem(this.itemBuilder.toItemStack()).setByteArray(tag, b).build());
         return this;
     }
 
     public byte[] getByteArray(String tag){
-        return new RNBItem(this.RItemBuilder.toItemStack()).getByteArray(tag);
+        return new RNBItem(this.itemBuilder.toItemStack()).getByteArray(tag);
     }
 
     public RItemUnsafe setIntArray(String tag, int[] b){
-        this.RItemBuilder = new ItemBuilder(new RNBItem(this.RItemBuilder.toItemStack()).setIntArray(tag, b).build());
+        this.itemBuilder = new ItemBuilder(new RNBItem(this.itemBuilder.toItemStack()).setIntArray(tag, b).build());
         return this;
     }
 
     public int[] getIntArray(String tag){
-        return new RNBItem(this.RItemBuilder.toItemStack()).getIntArray(tag);
+        return new RNBItem(this.itemBuilder.toItemStack()).getIntArray(tag);
     }
 
     public boolean containsTag(String s){
-        return new RNBItem(this.RItemBuilder.toItemStack()).containsTag(s);
+        return new RNBItem(this.itemBuilder.toItemStack()).containsTag(s);
     }
 
     public RItemUnsafe remove(String tag){
-        this.RItemBuilder = new ItemBuilder(new RNBItem(this.RItemBuilder.toItemStack()).removeTag(tag).build());
+        this.itemBuilder = new ItemBuilder(new RNBItem(this.itemBuilder.toItemStack()).removeTag(tag).build());
         return this;
     }
 
     public ItemBuilder toItemBuilder(){
-        return this.RItemBuilder;
+        return this.itemBuilder;
     }
 }
